39f5b99dfd66f73368d44c99a08372e6bcaa36c4
Aleksandr Stashchuk <alexander.stashchuk@gmail.com>
2024-08-15 16:20:07 +0300

    changes trahsh files

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,3 @@
-# def main():
-#     print('Welcome to the Brain Games!')
 import prompt
 
 
@@ -7,7 +5,4 @@ def welcome_user():
     print('Welcome to the Brain Games!')
     user_name = prompt.string(prompt='May I have your name? ', empty=False)
     print(f'Hello, {user_name}!')
-
-
-# # if __name__ == '__main__':
-#     main()
+    
--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-# from brain_games.cli import welcome_user
 import brain_games.cli
 
 
 def main():
-    # print('Welcome to the Brain Games!')
-    # welcome_user()
     brain_games.cli.welcome_user()
-
-
-def greet(who):
-    print(f'Hello, {who}!')
 
 
 if __name__ == '__main__':
